17b273d57a6db6fb5ba2f5165b5d297dfe26ee1c
flaamjab <flaamjab@gmail.com>
2020-05-29 20:17:41 +0500

    fix test_run.py one more time

--- src/test_run.py
+++ src/test_run.py
@@ -1,2 +1,3 @@
-from frontend.ui_components import *
-from data import *
+from data import db, activities, activity, user
+from frontend.ui_components import main_menu, stats_display
+from frontend.ui_components import util
