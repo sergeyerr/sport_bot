1354a30f75f68bb0997266ef5e177c292961a243
flaamjab <flaamjab@gmail.com>
2020-05-29 20:25:12 +0500

    fix test_run.py for the last time (hopefully)

--- src/frontend/setup.py
+++ src/frontend/setup.py
@@ -7,9 +7,10 @@ telebot.logger.setLevel(logging.INFO)
 
 token_path = "resources/telegram_bot_token"
 
-try:
-    with open(token_path) as f:
-        t = f.readline()
-    frontend = telebot.TeleBot(t)
-except Exception as e:
-    logger.exception(e)
+if __name__ == "__main__":
+    try:
+        with open(token_path) as f:
+            t = f.readline()
+        frontend = telebot.TeleBot(t)
+    except Exception as e:
+        logger.exception(e)
