25f55ebc43b96a36dce8bdd0ab78375a637aa4b3
Dmitry Vlasov <dima.vlasov@icloud.com>
2020-06-04 01:50:54 +0500

    Add distance and geoposition

--- src/frontend/scenarios/activity_creation.py
+++ src/frontend/scenarios/activity_creation.py
@@ -1,12 +1,10 @@
 import telebot
 from telebot import types
-#from frontend.setup import frontend
+# from frontend.setup import frontend
 from data.activity import Activity
 from bot_core import bot
 
-
 frontend = telebot.TeleBot('1209584769:AAFqOGx-Vl28QDG_rFHhR2PosOO3DxSHmnk')
-
 
 new_activity = Activity()
 
@@ -30,15 +28,16 @@ def launch_scenario(message):
     frontend.register_next_step_handler(message, type_step)
 
 
-# Пользователь выбирает тип мероприятия
+# Обработка указанного типа мероприятия
 def type_step(message):
     type = message.text
 
     if type in ('Ходьба', 'Бег', 'Велопрогулка', 'Велоспорт'):
         new_activity.type = type
+
         frontend.send_message(
             message.chat.id,
-            'Введите дистанцию',
+            'Введите дистанцию в км',
             reply_markup=types.ReplyKeyboardRemove())
         frontend.register_next_step_handler(message, distance_step)
     else:
@@ -49,7 +48,63 @@ def type_step(message):
         return
 
 
+# Проверка на число
+def is_digit(string):
+    if string.isdigit():
+        return True
+    else:
+        try:
+            float(string)
+            return True
+        except ValueError:
+            return False
+
+
+# Обработка указанной дистанции
 def distance_step(message):
+    distance = message.text
+
+    if is_digit(distance):
+        new_activity.distance = float(distance)
+
+        frontend.send_message(
+            message.chat.id,
+            'Отправьте место проведения мероприятия')
+        frontend.register_next_step_handler(message, distance_step)
+    else:
+        msg = frontend.reply_to(
+            message,
+            'Дистанция должна быть числом. Пожалуйста, повторите попытку')
+        frontend.register_next_step_handler(msg, distance_step)
+        return
+
+
+# Обработка указанной геопозиции
+def coord_step(message):
+    coord = message.location
+
+    if not (coord is None):
+        new_activity.x = message.location.longitude
+        new_activity.y = message.location.latitude
+
+        frontend.send_message(
+            message.chat.id,
+            'Введите дату и время в формате ДД/ММ/ГГ ЧЧ:ММ')
+        frontend.register_next_step_handler(message, time_step)
+    else:
+        msg = frontend.reply_to(
+            message,
+            'Пожалуйста, отправьте геопозицию')
+        frontend.register_next_step_handler(msg, coord_step)
+        return
+
+
+def time_step(message):
+    pass
+
+
+# Если все ок, высылает главное меню
+def finalize(message):
     pass
 
 
