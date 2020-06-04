4d68971cb45b342020038d74249ff6da779afbca
flaamjab <flaamjab@gmail.com>
2020-06-05 02:33:10 +0500

    this should be the final fix

--- src/test_run.py
+++ src/test_run.py
@@ -4,6 +4,3 @@ from data import db, activities, activity, user
 from frontend.ui_components import main_menu, stats_display
 from frontend import util
 from frontend import setup
-
-if len(argv) == 2:
-    setup.create_frontend(argv[1])
