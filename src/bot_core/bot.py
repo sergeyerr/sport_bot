d26fb24c9d5ffa127108ffb201ee29c2b8663cb6
geomPhantom <antonzubarev2@gmail.com>
2020-06-05 02:55:58 +0500

    Activity viewer with join method + some db functions

--- src/bot_core/bot.py
+++ src/bot_core/bot.py
@@ -1,6 +1,6 @@
 from data.user import User
 from data.activity import Activity
-
+from data.activities import Activities
 
 def get_user_by_id(user_id):
     query = User.select().where(User.id == user_id)
@@ -23,3 +23,20 @@ def save_user(new_user):
             new_user.gender, new_user.city, new_user.x, new_user.y)
     query = User.insert(data, fields=fields).execute()
     return query
+
+
+#  для тестирования!! по факту надо учитывать user_id
+def get_all_activities():
+    return list(Activity.select())
+
+
+def participate_in_activity(user_id, activity_id):
+    Activities.insert(user_id=user_id, activity_id=activity_id).execute()
+
+
+def quit_activity(user_id, activity_id):
+    Activities.delete().where((Activities.activity_id == activity_id) & (Activities.user_id == user_id)).execute()
+
+
+def is_participating(user_id, activity_id):
+    return (Activities.select().where((Activities.activity_id == activity_id) & (Activities.user_id == user_id))).exists()
--- src/frontend/ui_components/activity_viewer.py
+++ src/frontend/ui_components/activity_viewer.py
@@ -5,15 +5,10 @@
 
 from frontend.setup import frontend
 from bot_core.bot import get_all_activities
+from bot_core.bot import participate_in_activity, quit_activity, is_participating
 from telebot import types
 # Запись на мероприятие должна добавлять соответствующую запись
 # в таблицу activities.
-
-activity = {
-    "name": "Поездка на 68 автобусе",
-    # Остальные поля из прототипа интерфеса в Figma
-    # надо взять.
-}
 
 all_activities = get_all_activities()
 pointer = 0
@@ -50,9 +45,14 @@ def get_activities_markup():
         types.InlineKeyboardButton(
             text='Предыдущий',
             callback_data=f'activity_viewer_prev_{pointer - 1}'))
+
+    join_button_text = 'Присоединиться'
+    if is_participating(1, pointer):
+        join_button_text = 'Покинуть'
+
     buttons.append(
         types.InlineKeyboardButton(
-            text='Присоединиться',
+            text=join_button_text,
             callback_data=f'join_activity_{pointer}'))
     buttons.append(
         types.InlineKeyboardButton(
@@ -65,7 +65,7 @@ def get_activities_markup():
     buttons.append(
         types.InlineKeyboardButton(
             text='Участники',
-            callback_data=f'participants'))
+            callback_data=f'participants_{pointer}'))
     markup.add(*buttons)
     return markup
 
@@ -96,6 +96,33 @@ def __prev_button_pressed(call):
     func=lambda call: call.data.startswith("activity_viewer_next"))
 def __next_button_pressed(call):
     __update_markup(call)
+    frontend.answer_callback_query(call.id)
+
+
+@frontend.callback_query_handler(
+    func=lambda call: call.data.startswith("join_activity"))
+def __join_button_pressed(call):
+    if is_participating(1, int(call.data.split('_')[2])):
+        quit_activity(1, int(call.data.split('_')[2]))
+    else:
+        participate_in_activity(1, int(call.data.split('_')[2]))
+
+    frontend.edit_message_reply_markup(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                       reply_markup=get_activities_markup())
+    frontend.answer_callback_query(call.id)
+
+
+@frontend.callback_query_handler(
+    func=lambda call: call.data.startswith("go_back"))
+def __go_back_button_pressed(call):
+    # ЗАГРУЗИТЬ МЕНЮ
+    frontend.answer_callback_query(call.id)
+
+
+@frontend.callback_query_handler(
+    func=lambda call: call.data.startswith("participants"))
+def __participants_button_pressed(call):
+    #ВЫВЕСТИ СПИСОК УЧАСТНИКОВ СОБЫТИЯ (ПО ТАБЛИЦЕ ACTIVITIES)
     frontend.answer_callback_query(call.id)
 
 
