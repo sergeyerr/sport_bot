2052e5f5c505e538b66281978a25a8f6c1f68990
flaamjab <flaamjab@gmail.com>
2020-06-05 13:05:00 +0500

    fix path in db.py

--- src/data/db.py
+++ src/data/db.py
@@ -11,7 +11,7 @@ if os.environ.get('POSTGRES_USER'):
 else:
     # SQLite database using WAL journal mode and 64MB cache.
     database = SqliteDatabase(
-        database='../resources/bot_persistence.db',
+        database='resources/bot_persistence.db',
         pragmas={
             'journal_mode': 'wal',
             'cache_size': -1024 * 64
