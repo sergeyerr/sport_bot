fcc4d324529cd7264d6af128e7ea951361df0593
flaamjab <flaamjab@gmail.com>
2020-06-05 13:03:18 +0500

    move all bot functions to bot.py

--- src/bot_core/bot.py
+++ src/bot_core/bot.py
@@ -1,6 +1,7 @@
+from geopy.distance import distance
 from data.user import User
+from data.activities import Activities
 from data.activity import Activity
-
 
 def get_user_by_id(user_id):
     try:
@@ -11,7 +12,9 @@ def get_user_by_id(user_id):
 
 def user_exists(user_id):
     try:
+        print(user_id)
         User.get(User.id == user_id)
+        print("TRUE")
         return True
     except Exception:
         return False
@@ -28,13 +31,54 @@ def create_user(new_user):
     #         new_user.gender, new_user.city, new_user.x, new_user.y)
     # query = User.insert(data, fields=fields).execute()
     # return query
-    new_user.save(force_insert=True)
+    return new_user.save(force_insert=True)
 
 
-def save_activity(new_activity):
-    fields = [Activity.type, Activity.distance, Activity.date,
-              Activity.x, Activity.y]
-    data = (new_activity.type, new_activity.distance,
-            new_activity.date, new_activity.x, new_activity.y)
-    query = Activity.insert(data, fields=fields).execute()
-    return query
+def create_activity(new_activity):
+    # fields = [Activity.type, Activity.distance, Activity.date,
+    #           Activity.x, Activity.y]
+    # data = (new_activity.type, new_activity.distance,
+    #         new_activity.date, new_activity.x, new_activity.y)
+    # query = Activity.insert(data, fields=fields).execute()
+    # return query
+    return new_activity.save(force_insert=True)
+
+
+def suggest_activities(user_id, radius=5.0):
+    """
+        Находит мероприятия в округе
+    """
+
+    activity_ids = list(Activities.select().where(
+        Activities.user_id == user_id))
+    activities = list(Activity.select().where(
+        Activity.id.not_in([act.activity_id for act in activity_ids])))
+    our_buddy = User.get(User.id == user_id)
+    our_location = (our_buddy.x, our_buddy.y)
+    activities_filtered = list(filter(
+        lambda act: distance(our_location, (act.x, act.y)).km <= radius,
+        activities))
+    activities_sorted = sorted(
+        activities_filtered,
+        key=lambda act: distance(our_location, (act.x, act.y)).km)
+
+    return activities_sorted
+
+
+def suggest_buddies(user_id, radius):
+    """
+        Находит пользователей в округе
+    """
+
+    other_buddies = list(User.select().where(User.id != user_id))
+    our_buddy = User.get(User.id == user_id)
+    our_location = (our_buddy.x, our_buddy.y)
+
+    other_buddies_sorted = sorted(
+        other_buddies,
+        key=lambda buddy: distance(our_location, (buddy.x, buddy.y)).km)
+    other_buddies_filtered = list(filter(
+        lambda buddy: distance(our_location, (buddy.x, buddy.y)).km <= radius,
+        other_buddies_sorted))
+
+    return other_buddies_filtered
--- src/bot_core/suggest_buddies.py
+++ src/bot_core/suggest_buddies.py
@@ -1,20 +1,7 @@
 from geopy.distance import distance
 from data.user import User
 
-eps = 5.0  # radius of our neighbourhood
+radius = 5.0  # radius of our neighbourhood
 
 
-# Метод должен возвращать список словарей, содержащих
-# информацию о пользователях.
-def suggest_buddies(user_id):
-    other_buddies = list(User.select().where(User.id != user_id))
-    our_buddy = User.get(User.id == user_id)
-    our_location = (our_buddy.x, our_buddy.y)
 
-    other_buddies_sorted = sorted(
-        other_buddies,
-        key=lambda buddy: distance(our_location, (buddy.x, buddy.y)).km)
-    other_buddies_filtered = list(filter(
-        lambda buddy: distance(our_location, (buddy.x, buddy.y)).km <= eps,
-        other_buddies_sorted))
-    return other_buddies_filtered
