8899dcc9ef844c37791c93216231df77ad85b6e7
flaamjab <flaamjab@gmail.com>
2020-06-06 05:39:34 +0500

    make user_viewer and user_profile_viewer functional

--- src/frontend/util.py
+++ src/frontend/util.py
@@ -10,7 +10,7 @@ def user_picture(user_id):
         # Берем айдишник получившегося фото
         file_id = pictureMas[-1].file_id
     # Случай, когда нет аватарки (мб выдавать месс, мол, поставьте аву, пж?)
-    except IndexError:
+    except Exception:
         # Пока будем выдавать знак вопроса с локалки
-        file_id = open("resources/images/noavatar.jpg", 'rb')
+        file_id = open("resources/noavatar.jpg", 'rb')
     return file_id
